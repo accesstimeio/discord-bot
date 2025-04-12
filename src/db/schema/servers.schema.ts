c80b19dbbd74fa25cb31e3dde5f95e7e0434df8a
mehmetraufoguz <88249309+mehmetraufoguz@users.noreply.github.com>
2025-04-12 23:09:01 +0300

    chore: update schemas

--- src/db/schema/servers.schema.ts
+++ src/db/schema/servers.schema.ts
@@ -3,11 +3,13 @@ import { boolean, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 export const servers = pgTable("servers", {
     id: serial("id").primaryKey(),
     discordServerId: text("discord_server_id").notNull().unique(),
-    accessTimeProjectId: text("access_time_project_id"),
+    accessTimeProjectId: text("accesstime_project_id"),
+    accessTimeChainId: text("accesstime_chain_id"),
     verificationSignature: text("verification_signature"),
     isVerified: boolean("is_verified").default(false),
     subscriberRoleId: text("subscriber_role_id"),
     lastSyncAt: timestamp("last_sync_at"),
+    nonce: text("nonce"), // Used for signature verification
     createdAt: timestamp("created_at").defaultNow(),
     updatedAt: timestamp("updated_at").defaultNow()
 });
--- src/db/schema/users.schema.ts
+++ src/db/schema/users.schema.ts
@@ -4,7 +4,7 @@ export const users = pgTable("users", {
     id: serial("id").primaryKey(),
     serverId: integer("server_id").notNull(), // Reference to servers table
     discordId: text("discord_id").notNull().unique(),
-    walletAddress: text("wallet_address").unique(),
+    walletAddress: text("wallet_address"),
     nonce: text("nonce"), // Used for signature verification
     createdAt: timestamp("created_at").defaultNow(),
     updatedAt: timestamp("updated_at").defaultNow()
